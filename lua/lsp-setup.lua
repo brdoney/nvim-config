b72a369fb75dfb6d1b6969d9d239ed07e7957645
Brendan Doney <bre.doney@gmail.com>
2022-11-30 05:04:12 -0500

    Put snippets after other completions

--- lua/lsp-setup.lua
+++ lua/lsp-setup.lua
@@ -88,6 +88,8 @@ lspkind.init({
 
 -- Cmp {{{
 local cmp = require('cmp')
+local compare = require('cmp.config.compare')
+local types = require('cmp.types')
 local feedkeys = require('cmp.utils.feedkeys')
 local keymap = require('cmp.utils.keymap')
 
@@ -153,7 +155,43 @@ cmp.setup({
       end
     end, { "i", "s" }),
   }),
-  sources = cmp.config.sources({
+  sorting = {
+    priority_weight = 2,
+    comparators = {
+      -- Put snippets below everything else
+      function(entry1, entry2)
+        local kind1 = entry1:get_kind()
+        kind1 = kind1 == types.lsp.CompletionItemKind.Text and 100 or kind1
+        local kind2 = entry2:get_kind()
+        kind2 = kind2 == types.lsp.CompletionItemKind.Text and 100 or kind2
+        if kind1 ~= kind2 then
+          if kind1 == types.lsp.CompletionItemKind.Snippet then
+            return false
+          end
+          if kind2 == types.lsp.CompletionItemKind.Snippet then
+            return true
+          end
+          local diff = kind1 - kind2
+          if diff < 0 then
+            return true
+          elseif diff > 0 then
+            return false
+          end
+        end
+      end,
+      -- Default values from here down
+      compare.offset,
+      compare.exact,
+      -- compare.scopes,
+      compare.score,
+      compare.recently_used,
+      compare.locality,
+      compare.kind,
+      compare.sort_text,
+      compare.length,
+      compare.order,
+    }
+  }, sources = cmp.config.sources({
     { name = 'nvim_lsp' },
     { name = 'nvim_lsp_signature_help' },
     { name = 'vsnip' },
