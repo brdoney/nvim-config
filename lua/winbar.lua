ced37234a51f7300f5004b7621c204447103db84
Brendan Doney <bre.doney@gmail.com>
2024-01-17 20:06:17 -0500

    Move is-ignored functions and winbar

--- lua/winbar.lua
+++ lua/winbar.lua
@@ -2,7 +2,84 @@ local M = {}
 
 -- Don't do anything if we're in firenvim
 if vim.g.started_by_firenvim then
-  return
+  return M
+end
+
+-- From https://github.com/b0o/incline.nvim/blob/fdd7e08a6e3d0dd8d9aa02428861fa30c37ba306/lua/incline/util.lua
+
+local config = {
+  ignore = {
+    buftypes = "special",
+    filetypes = {},
+    floating_wins = true,
+    unlisted_buffers = true,
+    wintypes = "special"
+  },
+}
+
+local function is_ignored_filetype(filetype)
+  local ignore = config.ignore
+  return ignore.filetypes and vim.tbl_contains(ignore.filetypes, filetype)
+end
+
+local function is_ignored_buf(bufnr)
+  bufnr = bufnr or 0
+  local ignore = config.ignore
+  if ignore.unlisted_buffers and not vim.api.nvim_buf_get_option(bufnr, 'buflisted') then
+    return true
+  end
+  if ignore.buftypes then
+    local buftype = vim.api.nvim_buf_get_option(bufnr, 'buftype')
+    if ignore.buftypes == 'special' and buftype ~= '' then
+      return true
+    elseif type(ignore.buftypes) == 'table' then
+      if vim.tbl_contains(ignore.buftypes, buftype) then
+        return true
+      end
+    elseif type(ignore.buftypes) == 'function' then
+      if ignore.buftypes(bufnr, buftype) then
+        return true
+      end
+    end
+  end
+  if ignore.filetypes then
+    local filetype = vim.api.nvim_buf_get_option(bufnr, 'filetype')
+    if is_ignored_filetype(filetype) then
+      return true
+    end
+  end
+  return false
+end
+
+local function is_floating_win(winid)
+  return vim.api.nvim_win_get_config(winid or 0).relative ~= ''
+end
+
+local function is_ignored_win(winid)
+  winid = winid or 0
+  local bufnr = vim.api.nvim_win_get_buf(winid)
+  if is_ignored_buf(bufnr) then
+    return true
+  end
+  local ignore = config.ignore
+  if ignore.floating_wins and is_floating_win(winid) then
+    return true
+  end
+  if ignore.wintypes then
+    local wintype = vim.fn.win_gettype(winid)
+    if ignore.wintypes == 'special' and wintype ~= '' then
+      return true
+    elseif type(ignore.wintypes) == 'table' then
+      if vim.tbl_contains(ignore.wintypes, wintype) then
+        return true
+      end
+    elseif type(ignore.wintypes) == 'function' then
+      if ignore.wintypes(winid, wintype) then
+        return true
+      end
+    end
+  end
+  return false
 end
 
 M.get_filename = function()
@@ -31,7 +108,7 @@ vim.api.nvim_create_autocmd('BufWinEnter', {
   callback = function()
     -- Schedule so there's enough time for nobuflisted and other things to be processed for the current window
     vim.schedule(function()
-      if not require('is-ignored').is_ignored_win() then
+      if not is_ignored_win() then
         vim.opt_local.winbar =
         "%{%v:lua.require'nvim-navic'.get_location()%} %= %{%v:lua.require'winbar'.get_filename()%}"
       end
