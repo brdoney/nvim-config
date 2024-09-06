b71f4661828b5cf3144d3d10141a910425a0f7f0
Brendan Doney <bre.doney@gmail.com>
2024-09-06 14:14:02 -0400

    Add mapping to open focused DAP element in float

--- lua/plugins/debug.lua
+++ lua/plugins/debug.lua
@@ -54,9 +54,9 @@ return {
     config = function(_, opts)
       local dap, dapui = require("dap"), require("dapui")
 
-      vim.print(opts)
       dapui.setup(opts)
 
+      -- Close NvimTree and open dapui when DAP is started
       dap.listeners.before.attach.dapui_config = function()
         require('nvim-tree.api').tree.close()
         dapui.open()
@@ -65,6 +65,7 @@ return {
         require('nvim-tree.api').tree.close()
         dapui.open()
       end
+      -- A little annoying, since it hides results
       -- dap.listeners.before.event_terminated.dapui_config = function()
       --   dapui.close()
       -- end
@@ -72,9 +73,66 @@ return {
       --   dapui.close()
       -- end
 
+      local elements = { "scopes", "stacks", "repl", "watches", "breakpoints", "console" }
+      --- Keys are filetypes and values are the corresponding element (e.g. dapui_scopes="scopes")
+      local element_fts = {}
+      for _, element in ipairs(elements) do
+        local ft = "dapui_" .. element
+        element_fts[ft] = element
+      end
+
+      --- Converts a string to title case
+      ---@param str string the string to capitalise in title case
+      ---@return string the string in title case
+      local function title_case(str)
+        -- Special case for REPL
+        if str == "repl" then
+          return str:upper()
+        end
+        return str:sub(1, 1):upper() .. str:sub(2):lower()
+      end
+
+      --- Expand the currently focused DAP element
+      local function expand_dap_element()
+        local ft = vim.bo.filetype
+        local el = element_fts[ft]
+
+        -- If we're not in a DAP element, do nothing
+        if el == nil then
+          return
+        end
+
+        -- Same as Telescope
+        local width = math.floor(vim.o.columns * 0.8)
+        local height = math.floor(vim.o.lines * 0.9)
+
+        -- Open it in a floating window
+        require("dapui").float_element(el,
+          {
+            title = "DAP " .. title_case(el),
+            width = width,
+            height = height,
+            enter = true,
+            position = "center"
+          })
+      end
+
+      local dap_group = vim.api.nvim_create_augroup("dap", {})
+
+      -- Mappings to expand the currently focused DAP element
+      vim.api.nvim_create_autocmd("FileType", {
+        pattern = vim.tbl_keys(element_fts),
+        group = dap_group,
+        callback = function()
+          vim.keymap.set("n", "<leader>m", expand_dap_element, { buffer = true, desc = "Maximise DAP element" })
+          vim.keymap.set("n", "<leader>M", expand_dap_element, { buffer = true, desc = "Maximise DAP element" })
+        end
+      })
+
+      -- Add completion to DAP REPL
       vim.api.nvim_create_autocmd("FileType", {
         pattern = 'dap-repl',
-        group = vim.api.nvim_create_augroup("dap", {}),
+        group = dap_group,
         callback = function()
           require('dap.ext.autocompl').attach()
         end
