81f7ce8ec3c0a8034a2117f7371c51aa3bf433b0
Brendan Doney <bre.doney@gmail.com>
2025-07-31 10:16:44 -0400

    Update plugins

--- lua/plugins/keybinds.lua
+++ lua/plugins/keybinds.lua
@@ -1,9 +1,37 @@
 return {
   {
-    -- Resize mode using natural directions
     'mrjones2014/smart-splits.nvim',
-    opts = {},
-    keys = { { "<C-w>r", function() require('smart-splits').start_resize_mode() end, desc = "Start buffer resize mode" } }
+    event = 'VeryLazy',
+    dependencies = {
+      'pogyomo/submode.nvim',
+    },
+    config = function()
+      -- Resize
+      local submode = require 'submode'
+      submode.create('WinResize', {
+        mode = 'n',
+        enter = '<C-w>r',
+        leave = { '<Esc>', 'q', '<C-c>' },
+        hook = {
+          on_enter = function()
+            vim.notify 'Use { h, j, k, l } or { <Left>, <Down>, <Up>, <Right> } to resize the window'
+          end,
+          on_leave = function()
+            vim.notify ''
+          end,
+        },
+        default = function(register)
+          register('h', require('smart-splits').resize_left, { desc = 'Resize left' })
+          register('j', require('smart-splits').resize_down, { desc = 'Resize down' })
+          register('k', require('smart-splits').resize_up, { desc = 'Resize up' })
+          register('l', require('smart-splits').resize_right, { desc = 'Resize right' })
+          register('<Left>', require('smart-splits').resize_left, { desc = 'Resize left' })
+          register('<Down>', require('smart-splits').resize_down, { desc = 'Resize down' })
+          register('<Up>', require('smart-splits').resize_up, { desc = 'Resize up' })
+          register('<Right>', require('smart-splits').resize_right, { desc = 'Resize right' })
+        end,
+      })
+    end,
   },
   {
     "folke/which-key.nvim",
