99aceb8fe1074e0ac8f66145423383d945a79678
Brendan Doney <brendan.doney@studentfirst.com>
2025-10-09 16:08:33 -0400

    Move rust ftplugin file to lua

---
+++ after/ftplugin/rust.lua
@@ -0,0 +1,3 @@
+-- Override the mapping for :RustRun
+vim.keymap.set("n", "<D-r>", "<leader>r")
+vim.keymap.set("!", "<D-r>", "<leader>r")
--- after/ftplugin/rust.vim
+++
@@ -1,4 +0,0 @@
-" Override the mapping for :RustRun
-nmap <D-r> <leader>r
-map! <D-r> <leader>r
-
