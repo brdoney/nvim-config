8654d328bafbdda3739ceac82e8f5d5c56e09eba
Brendan Doney <brendan.doney@studentfirst.com>
2026-07-15 13:15:58 -0400

    Use keys section for barbar

--- lua/plugins/tabline.lua
+++ lua/plugins/tabline.lua
@@ -20,44 +20,45 @@ return {
     cond = not vim.g.started_by_firenvim,
     dependencies = {
       'nvim-tree/nvim-web-devicons', -- OPTIONAL: for file icons
+      'lewis6991/gitsigns.nvim', -- OPTIONAL: for git status
     },
     init = function()
       -- Lazy will set it up for us
       vim.g.barbar_auto_setup = false
-
-      local function barbarmap(key, command, desc)
-        local opts = { silent = true, noremap = true, desc = desc }
-        vim.keymap.set("n", key, command, opts)
-      end
-
-      -- Barbar operations
-      -- barbarmap("<leader>p", ":BufferPick<CR>", "Pick buffer")
-      barbarmap("<leader>[", ":BufferPrevious<CR>", "Previous buffer")
-      barbarmap("<leader>]", ":BufferNext<CR>", "Next buffer")
-      barbarmap("<leader>{", ":BufferMovePrevious<CR>", "Move buffer left")
-      barbarmap("<leader>}", ":BufferMoveNext<CR>", "Move buffer right")
-      barbarmap("<leader>\\", ":BufferClose<CR>", "Close buffer")
+    end,
+    keys = {
+      -- Buffer operations
+      -- { "<leader>p", "<Cmd>BufferPick<CR>", desc = "Pick buffer" },
+      { "<leader>[", "<Cmd>BufferPrevious<CR>", desc = "Previous buffer" },
+      { "<leader>]", "<Cmd>BufferNext<CR>", desc = "Next buffer" },
+      { "<leader>{", "<Cmd>BufferMovePrevious<CR>", desc = "Move buffer left" },
+      { "<leader>}", "<Cmd>BufferMoveNext<CR>", desc = "Move buffer right" },
+      { "<leader>\\", "<Cmd>BufferClose<CR>", desc = "Close buffer" },
 
       -- Tabpage operations
-      barbarmap("<leader>tt", ":tabnew<CR>", "Open new tab")
-      barbarmap("<leader>t[", ":tabprevious<CR>", "Previous tab")
-      barbarmap("<leader>t]", ":tabnext<CR>", "Next tab")
-      barbarmap("<leader>|", function()
-        if vim.g.dbui_tab ~= nil and vim.api.nvim_get_current_tabpage() == vim.g.dbui_tab then
-          -- If we're currently in dbee, close it. Otherwise, it blocks tabclose
-          require("dbee").close()
-          vim.g.dbui_tab = nil
-        end
+      { "<leader>tt", "<Cmd>tabnew<CR>", desc = "Open new tab" },
+      { "<leader>t[", "<Cmd>tabprevious<CR>", desc = "Previous tab" },
+      { "<leader>t]", "<Cmd>tabnext<CR>", desc = "Next tab" },
+      {
+        "<leader>|",
+        function()
+          if vim.g.dbui_tab ~= nil and vim.api.nvim_get_current_tabpage() == vim.g.dbui_tab then
+            -- If we're currently in dbee, close it. Otherwise, it blocks tabclose
+            require("dbee").close()
+            vim.g.dbui_tab = nil
+          end
 
-        -- ":tabclose<CR>"
-        vim.cmd.tabclose()
-        -- Reload nvim tree for git updates
-        require('nvim-tree.api').tree.reload()
-        -- Refresh gitgutter for updates
-        -- vim.cmd("GitGutterAll")
-        require('gitsigns').refresh()
-      end, "Close tab")
-    end,
+          -- ":tabclose<CR>"
+          vim.cmd.tabclose()
+          -- Reload nvim tree for git updates
+          require('nvim-tree.api').tree.reload()
+          -- Refresh gitgutter for updates
+          -- vim.cmd("GitGutterAll")
+          require('gitsigns').refresh()
+        end,
+        desc = "Close tab",
+      },
+    },
     opts = {
       auto_hide = 1,
       focus_on_close = "previous",
